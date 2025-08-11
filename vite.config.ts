b7d8e545510f58c18c6a342d61462e4d317bff31
geolunalg <geovanni_luna@yahoo.com>
2025-08-11 13:45:54 +0000

    add ui.shadcn components

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,19 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    host: "0.0.0.0",
+    watch: {
+      usePolling: true
+    }
+  },
+  // resolve: {
+  //   alias: {
+  //     "~": path.resolve(__dirname, "./app"),
+  //   }
+  // }
 });
