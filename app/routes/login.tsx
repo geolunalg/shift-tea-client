7b5b551f22018a1b9b347be4486a086c2d259f8e
geolunalg <geovanni_luna@yahoo.com>
2025-08-11 19:56:23 +0000

    add Login/Register currently both, need to update register

---
+++ app/routes/login.tsx
@@ -0,0 +1,6 @@
+import LoginForm from "~/welcome/Login";
+
+
+export default function Login() {
+    return <LoginForm />;
+}
---
+++ app/routes/register.tsx
@@ -0,0 +1,6 @@
+import RegisterForm from "~/welcome/Register";
+
+
+export default function Register() {
+    return <RegisterForm />;
+}
