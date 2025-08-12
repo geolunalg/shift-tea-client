618432f3202c3307673e9f0458ecc343d38671bc
geolunalg <geovanni_luna@yahoo.com>
2025-08-12 15:12:18 +0000

    removed move base files

--- app/components/register-form.tsx
+++ app/components/register-form.tsx
@@ -4,7 +4,6 @@ import { Card, CardContent } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 
-import placeholder from "./placeholder.svg";
 import shifttee from "./shifttee.svg";
 import { Link } from "react-router";
 
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,4 +1,13 @@
 import LoginForm from "~/pages/Login";
+import type { Route } from "./+types/home";
+
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+    { author: "Geovanni Luna" }
+  ];
+}
 
 export default function Login() {
   return <LoginForm />;
